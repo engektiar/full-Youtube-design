8428a6bc2aae1cf638112000dd67ea07e47f5df1
Ektiar <eng.ektiar@gmail.com>
2022-05-24 23:07:49 +0600

    leftside doen

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const mysidebar = document.getElementById("sidebar");
-
 $(document).ready(function () {
   $("#btn").click(function () {
     $("#sidebar").toggle();
   });
 });
+
+function myFunction() {
+  var element = document.getElementById("showMore");
+  element.classList.toggle("moreactive");
+}
