64702d535ab0a39cd000b6612cea3723d3b4540c
Eng Md Ektiar Hossen <97474187+engektiar@users.noreply.github.com>
2022-05-25 18:01:10 +0600

    protom part done

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const mysidebar = document.getElementById("sidebar");
-$(document).ready(function () {
-  $("#btn").click(function () {
-    $("#sidebar").toggle();
-  });
-});
+//  const mysidebar = document.getElementById("sidebar");
+// $(document).ready(function () {
+//   $("#btn").click(function () {
+//     $("#sidebar").toggle();   }); });
 
+function mainFunction() {
+  var element = document.getElementById("sidebar");
+   
+  element.classList.toggle("ektiar");
+  
+  
+}
 function myFunction() {
   var element = document.getElementById("showMore");
   element.classList.toggle("moreactive");
